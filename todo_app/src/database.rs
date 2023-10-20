ecc448f98feb08a1481a6d155047422cc1d53dd0
pocoq <37747107+pocoq@users.noreply.github.com>
2023-10-20 10:19:58 +0700

    update

--- todo_app/src/database.rs
+++ todo_app/src/database.rs
@@ -1,18 +1,3 @@
-// //
-// use diesel::prelude::*;
-// use diesel::pg::PgConnection;
-// use dotenv::dotenv;
-// use std::env;
-// use crate::config::Config;
-
-// pub fn establish_connection() -> PgConnection {
-//     dotenv().ok();
-
-//     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-//     PgConnection::establish(&database_url).unwrap_or_else(|_| panic!("Error connecting to {}",
-//                                                                      database_url))
-// }
-
 use actix_web::dev::Payload;
 use actix_web::error::ErrorServiceUnavailable;
 use actix_web::{Error, FromRequest, HttpRequest};
